e2d24505ceb7442bd29b0b58b820d331e82f4efa
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-07-23 14:12:54 +0500

    Add password and approval controls to CustomUser admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -2,42 +2,37 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser, ProductCategory
 from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
+from .models import CustomUser, ProductCategory
+
 
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
+
+
+# users/admin.py
+from django.contrib import admin, messages
+from django.contrib.auth.hashers import make_password
+from .models import CustomUser
+from .utils import generate_random_password
+
+
 @admin.register(CustomUser)
-class CustomUserAdmin(BaseUserAdmin):
-    model = CustomUser
-    list_display = ('id', 'phone_number', 'full_name', 'is_approved', 'is_staff', 'is_superuser')
-    list_filter = ('is_active', 'is_approved', 'is_staff', 'is_superuser')
-    readonly_fields = ('last_login', 'date_joined')
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'is_active', 'is_approved']
+    actions = ['approve_and_generate_password']
 
-    fieldsets = (
-        (None, {'fields': ('phone_number', 'password')}),
-        (_('Personal info'), {'fields': ('full_name', 'project_name', 'category', 'address_name', 'address_lat', 'address_long')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_approved', 'is_staff', 'is_superuser', 'is_seller', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'phone_number', 'username', 'email', 'password1', 'password2',
-                'is_active', 'is_approved', 'is_staff', 'is_superuser', 'is_seller'
-            ),
-        }),
-    )
-
-    search_fields = ('phone_number', 'full_name')
-    ordering = ('-id',)
-
-    def save_model(self, request, obj, form, change):
-        newly_approved = not obj.is_approved and form.cleaned_data.get('is_approved', False)
-        super().save_model(request, obj, form, change)
-
-        if newly_approved:
-            # Bu joyda siz SMS yoki email yuborishingiz mumkin
-            print(f"Yuborish kerak: login={obj.phone_number}")
+    @admin.action(description="Tasdiqlash va parol yaratish")
+    def approve_and_generate_password(self, request, queryset):
+        for user in queryset:
+            if not user.is_active:
+                password = generate_random_password()
+                user.password = make_password(password)
+                user.is_active = True
+                user.is_approved = True
+                user.save()
+                messages.success(request, f"{user.username} uchun parol: {password}")
+            else:
+                messages.warning(request, f"{user.username} allaqachon aktiv.")
--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, ProductCategory
 
 
 class AddressSerializer(serializers.Serializer):
@@ -7,15 +7,47 @@ class AddressSerializer(serializers.Serializer):
     lat = serializers.FloatField()
     long = serializers.FloatField()
 
+
 class SellerRegistrationSerializer(serializers.ModelSerializer):
+    category_id = serializers.IntegerField(write_only=True)
+    address = AddressSerializer()
+
     class Meta:
         model = CustomUser
-        fields = ['username', 'phone_number', 'full_name', 'project_name', 'category', 'address_name', 'address_lat', 'address_long']
+        fields = ['id', 'full_name', 'project_name', 'category_id', 'phone_number', 'address', 'status']
 
     def create(self, validated_data):
-        return CustomUser.objects.create(
+        address_data = validated_data.pop('address')
+        category_id = validated_data.pop('category_id')
+        category = ProductCategory.objects.get(id=category_id)  # 'category_id' orqali 'ProductCategory'ni olish
+
+        user = CustomUser.objects.create(
             **validated_data,
+            category=category,
             is_active=False,
             is_approved=False,
             is_seller=True
         )
+
+        # Address ma'lumotlarini foydalanuvchiga qo‘shamiz
+        user.address_name = address_data['name']
+        user.address_lat = address_data['lat']
+        user.address_long = address_data['long']
+        user.save()
+
+        return user
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "full_name": instance.full_name,
+            "project_name": instance.project_name,
+            "category_id": instance.category.id if instance.category else None,
+            "phone_number": instance.phone_number,
+            "address": {
+                "name": instance.address_name,
+                "lat": instance.address_lat,
+                "long": instance.address_long,
+            },
+            "status": "pending"  # Response'da statusni ko‘rsatamiz
+        }
